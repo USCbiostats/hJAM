025893812d245f97207c57b708dc187dbfea0746
Lai Jiang <jian848@usc.edu>
2021-01-07 17:09:01 -0800

    remove unnecessary dependencies

--- R/SNPs_scatter_plot.R
+++ R/SNPs_scatter_plot.R
@@ -1,34 +1,31 @@
-#' Scatter plot for all the SNPs used in the analysis
-#' @description To generate the scatter plot of all the SNPs that the user use in the analysis
+#' Scatter plot for SNPs vs. one intermediate in the analysis
+#' @description To generate the scatter plot of the SNPs vs. one intermediate that the user use in the analysis
 #'
-#' @param A The effects of SNPs on the exposures (Gx).
+#' @param alphas The effects of SNPs on the intermediate (i.e. exposure/risk factor) (Gx).
 #' @param betas.Gy The betas in the paper: the marginal effects of SNPs on the phenotype (Gy)
-#' @param num_X The number of intermediates in the research question.
+#' @param X.label The label of the intermediate (i.e. exposure/risk factor). Default is NULL.
 #' @author Lai Jiang
 #'
 #' @return A set of scatter plots with x-axis being the conditional \eqn{\alpha}{alpha} estimates for each
 #' intermediate and y-axis being the \eqn{\beta}{beta} estimates.
 #' @export
-#' @import ggplot2 ggpubr
+#' @import ggplot2
 #' @examples
 #' data(MI)
-#' t = SNPs_scatter_plot(A = MI.Amatrix, betas.Gy = MI.betas.gwas, num_X = 2)
+#' t = SNPs_scatter_plot(alphas = MI.Amatrix[, 1], betas.Gy = MI.betas.gwas, X.label = "BMI")
 #' t
 
-SNPs_scatter_plot = function(A, betas.Gy, num_X){
-  plotList = list()
-  label.plot = colnames(A)
-  if(nrow(A) == length(betas.Gy)){
-    dta.plot = as.data.frame(cbind(A, betas.Gy))
-    for(i in 1:num_X){
-      plotList[[i]] = ggplot(dta.plot, aes(x = dta.plot[, i], y = betas.Gy)) + geom_point() +
-                     geom_hline(yintercept = 0, color = 'red') +
-                     xlab(expression(hat(alpha))) + ylab(expression(hat(beta))) + theme_bw()
-   }
-   p = ggarrange(plotlist=plotList, ncol=2, nrow=ceiling(num_X/2), common.legend = T, legend="bottom",
-                  labels = label.plot, font.label = list(size = 12))
+SNPs_scatter_plot = function(alphas, betas.Gy, X.label = NULL){
+  if(length(alphas) == length(betas.Gy)){
+    dta.plot = data.frame(cbind(alphas, betas.Gy))
+    p = ggplot(dta.plot, aes(x = alphas, y = betas.Gy)) + geom_point() +
+      geom_hline(yintercept = 0, color = 'red') +
+      xlab(expression(hat(alpha))) + ylab(expression(hat(beta))) + theme_bw()
+    if(!is.null(X.label)){
+      p = p + ggtitle(X.label)
+    }
    return(p)
   }else{
-    stop("The number of SNPs in A matrix is not the same as the number of SNPs in betas.Gy vector.")
+    stop("The number of SNPs in alphas is not the same as the number of SNPs in betas.Gy vector.")
   }
 }
