249c3588ea48eec81e65f28aa8a9c31ad520fe78
Lai Jiang <jian848@usc.edu>
2021-01-15 15:50:43 -0800

    add elastic net hJAM

--- R/EN.hJAM.R
+++ R/EN.hJAM.R
@@ -1,33 +1,33 @@
-#' Regularized hJAM
+#' Elastic net hJAM
 #' @description Function to implement regularized hJAM, including elastic net hJAM and lasso hJAM.
 #'
 #' @param betas.Gy The betas in the paper: the marginal effects of SNPs on the phenotype (Gy)
-#' @param betas_se.Gy The standard errors of the betas
 #' @param N.Gy The sample size of the GWAS where you obtain the betas.Gy and betas_se.Gy
 #' @param raf.Gy The reference allele frequency of the SNPs in betas.Gy
 #' @param Geno The individual level data of the reference panel. Must have the same order of SNPs as in the betas.Gy.
 #' @param A The conditional A matrix.
-#' @param selection_alg The selection algorithm. It can be SHA-JAM ("susie") or regularized hJAM ("en" for elastic net and "lasso" for LASSO).
+#' @param tune_glmnet The \eqn{\alpha} used in the glmnet R package to tune the shrinkage parameter. Default is 0.5.
 #' @param ridgeTerm Add a small elelment to the diagnoal of X'X to make the matrix invertable.
 #' @author Lai Jiang
 #'
 #' @return An object of the Regularized hJAM
 #'
 #' \describe{
-#'    \item{Exposure}{The intermediates, such as the modifiable risk factors in Mendelian Randomization and gene expression in transcriptome analysis.}
 #'    \item{numSNP}{The number of SNPs that the user use in the instrument set.}
-#'    \item{Estimate}{The conditional estimates of the associations between intermediates and the outcome.}
-#'    \item{StdErr}{The standard error of the conditional estimates of the associations between intermediates and the outcome.}
-#'    \item{Lower.CI}{The lower bound of the 95\% confidence interval of the estimates.}
-#'    \item{Upper.CI}{The upper bound of the 95\% confidence interval of the estimates.}
-#'    \item{Pvalue}{The p value of the estimates with a type-I error equals 0.05.}
+#'    \item{Selected_variable_length}{The number of selected intermediates, regardless of the credible sets.}
+#'    \item{Selected_variable_name}{The label/name for each selected intermediates.}
+#'    \item{Coefficients}{The coefficients of selected intermediates. Otherwise will be zero.}
 #' }
 #'
 #' @export
 #' @importFrom glmnet cv.glmnet
+#' @examples
+#' data(ENhJAM.SimulationSet)
+#' EN.hJAM(betas.Gy = Simulation.betas.gwas, N.Gy = 5000, raf.Gy = Simulation.maf.gwas,
+#' Geno = Simulation.Geno, A = Simulation.Amatrix, ridgeTerm = FALSE)
 
-reghJAM = function(betas.Gy, betas_se.Gy = NULL, N.Gy, raf.Gy = NULL,
-                  Geno, A, selection_alg = 'en', ridgeTerm = FALSE){
+EN.hJAM = function(betas.Gy, N.Gy, raf.Gy = NULL,
+                   Geno, A, tune_glmnet = 0.5, ridgeTerm = FALSE){
 
   # Check the dimension of betas.Gy, Geno and A
   dim_betas = length(betas.Gy)
@@ -40,7 +40,6 @@ reghJAM = function(betas.Gy, betas_se.Gy = NULL, N.Gy, raf.Gy = NULL,
     zero.A.row = ifelse(apply(A, 1, sum)==0, TRUE, FALSE)
     A = A[!zero.A.row, ]
     betas.Gy = betas.Gy[!zero.A.row]
-    betas_se.Gy = betas_se.Gy[!zero.A.row]
     Geno = Geno[, !zero.A.row]
 
     # Remove rows with zero in Genotype file
@@ -92,13 +91,8 @@ reghJAM = function(betas.Gy, betas_se.Gy = NULL, N.Gy, raf.Gy = NULL,
 
     zero.X.column = ifelse(apply(X, 2, sum)==0, TRUE, FALSE)
     X = X[, !zero.X.column]
-    if(selection_alg == 'en'){
-      tune_alpha = 0.5
-    }else if(selection_alg == 'lasso'){
-      tune_alpha = 1
-    }
 
-    glm.out = cv.glmnet(X, zL, family="gaussian", alpha = tune_alpha, intercept = FALSE) # set intercept=0, use elastic net
+    glm.out = cv.glmnet(X, zL, family="gaussian", alpha = tune_glmnet, intercept = FALSE) # set intercept=0, use elastic net
     betas.XY = coef(glm.out)@x
     i.XY = coef(glm.out)@i
     i.length = length(i.XY)
@@ -108,10 +102,9 @@ reghJAM = function(betas.Gy, betas_se.Gy = NULL, N.Gy, raf.Gy = NULL,
       Selected_variable_length = i.length,
       Selected_variable_index = i.XY,
       Selected_variable_name = colnames(A)[i.XY],
-      Coefficients = betas.XY,
-      Selection_algorithm = selection_alg)
+      Coefficients = betas.XY)
 
-    class(out) <- "SHAJAM"
+    class(out) <- "ENhJAM"
     return(out)
   }else{
     stop("The number of SNPs in betas.Gy, A matrix and the reference panel (Geno) are different.")
